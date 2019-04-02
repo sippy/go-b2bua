4dfd882766aa54f89c75e366073a34840347a15f
Andriy Pylypenko <bamby@sippysoft.com>
2019-04-02 17:49:29 +0300

    Use To4() instead of To16().

--- src/sippy/sdp/sdp_origin.go
+++ src/sippy/sdp/sdp_origin.go
@@ -81,7 +81,7 @@ func NewSdpOrigin(address string) (*SdpOrigin, error) {
         return nil, errors.New("The address is not IP address: " + address)
     }
     address_type := "IP4"
-    if ip.To16() != nil {
+    if ip.To4() == nil {
         address_type = "IP6"
     }
     sid := atomic.AddInt64(&_sdp_session_id, 1)
