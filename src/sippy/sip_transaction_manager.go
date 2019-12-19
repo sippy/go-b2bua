7049e7c219a1672bafa68150b182ecbc9d00c863
Andriy Pylypenko <bamby@sippysoft.com>
2019-12-19 15:18:01 +0200

    Remove discrepancy with the python version.

--- src/sippy/sip_transaction_manager.go
+++ src/sippy/sip_transaction_manager.go
@@ -559,12 +559,6 @@ func (self *sipTransactionManager) new_server_transaction(server sippy_net.Trans
             if req_receiver != nil {
                 rval = req_receiver.RecvRequest(req, t)
             }
-            if ua != nil {
-                self.consumers_lock.Lock()
-                consumers, _ = self.req_consumers[tid.CallId]
-                self.req_consumers[tid.CallId] = append(consumers, ua)
-                self.consumers_lock.Unlock()
-            }
         }
     }
     if rval == nil {
